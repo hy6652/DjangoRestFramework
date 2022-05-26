8d3e6e7ccbd6fb72ee3b1dba4f763a407e863948
고현영 <hyunyoungko@gohyeon-yeong-ui-MacBookAir.local>
2022-05-26 17:43:16 +0900

    ADD: add new view using mixins and generics

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 # from watchlist_app.api.views import movie_list, movie_details
-from watchlist_app.api.views import StreamPlaformAV, StreamDetailAV, WatchListAV, WatchDetailAV
+from watchlist_app.api.views import StreamPlaformAV, StreamDetailAV, WatchListAV, WatchDetailAV, ReviewList, ReviewDetail
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='movie-list'),
     path('<int:pk>', WatchDetailAV.as_view(), name='movie-detail'),
     path('stream/', StreamPlaformAV.as_view(), name='stream'),
     path('stream/<int:pk>', StreamDetailAV.as_view(), name='stream-detail'),
+    path('stream/<int:pk>/review', StreamDetailAV.as_view(), name='stream-detail'),
+    path('stream/review/<int:pk>', ReviewDetail.as_view(), name='review-detail'),
 ]
