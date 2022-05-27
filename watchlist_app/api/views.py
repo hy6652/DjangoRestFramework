914851059485f93e2945b4c42640094881d08adf
고현영 <hyunyoungko@gohyeon-yeong-ui-MacBookAir.local>
2022-05-27 16:42:35 +0900

    MODIFY: modify review model by adding user info

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import get_object_or_404
+
 from rest_framework            import status
 from rest_framework.response   import Response
 from rest_framework.views      import APIView
 from rest_framework            import generics
 from rest_framework            import viewsets
+from rest_framework.exceptions import ValidationError
 # from rest_framework import mixins
 # from rest_framework.decorators import api_view
 
@@ -14,10 +16,19 @@ from watchlist_app.api.serializers import ReviewSerializer, StreamPlatformSerial
 class ReviewCreate(generics.CreateAPIView):
     serializer_class = ReviewSerializer
 
+    def get_queryset(self):
+        return Review.objects.all()
+
     def perform_create(self, serializer):
         pk        = self.kwargs.get('pk')
         watchlist = WatchList.objects.get(pk=pk)
-        serializer.save(watchlist=watchlist)
+
+        review_user     = self.request.user
+        review_queryset = Review.objects.filter(watchlist=watchlist, review_user=review_user)
+
+        if review_queryset.exists():
+            raise ValidationError("You have already reviewed this movie")
+        serializer.save(watchlist=watchlist, review_user=review_user)
 
 
 class ReviewList(generics.ListAPIView):
